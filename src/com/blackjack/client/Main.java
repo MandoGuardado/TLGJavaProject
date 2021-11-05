886e1e4975a4904d11db4c908327ac8c31d87962
Armando Guardado <armando.guardado.ag@gmail.com>
2021-11-05 14:00:35 -0700

    Updated Main class to only handle the creation of Blackjack app and to trigger to start playing.

--- src/com/blackjack/client/Main.java
+++ src/com/blackjack/client/Main.java
@@ -1,4 +1,13 @@
 package com.blackjack.client;
 
+import com.blackjack.controller.BlackJackApp;
+
+
 class Main {
+
+    public static void main(String[] args) {
+
+        BlackJackApp blackjackapp = new BlackJackApp();
+        blackjackapp.playGame();
+    }
 }
