bca018c9299e7286c6c7dee8ba6d58d1bc680f8a
redrianhood <rian.boren@gmail.com>
2021-11-05 13:05:52 -0700

    Basic Test methods created for classes, minor edit to Player

---
+++ test/com/blackjack/PlayerTest.java
@@ -0,0 +1,30 @@
+package com.blackjack;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PlayerTest {
+    // fixtures
+    private Player player;
+
+    @Before
+    public void setUp(){
+        player = new Player("Bob", 0.0);
+        // new
+    }
+
+    @Test
+    public void hit_shouldReturnCard_whenCalled(){
+        // TODO
+    }
+
+    @Test
+    public void stand_shouldContinueGame_whenCalled() {
+        // TODO
+    }
+
+    @Test
+    public void bet_shouldSetBetValueAmount_whenCalled() {
+        // TODO
+    }
+}
