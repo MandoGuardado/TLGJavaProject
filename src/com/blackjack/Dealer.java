591621e3bfe1dc4f6990b4c10cc80af8824b0396
redrianhood <rian.boren@gmail.com>
2021-11-05 09:19:30 -0700

    Dealer class created - stubs

---
+++ src/com/blackjack/Dealer.java
@@ -0,0 +1,27 @@
+package com.blackjack;
+
+import java.util.Map;
+
+class Dealer {
+    private Deck deck;
+    private int cardTotal;
+    private Map<String, Card> cardsInHand;
+    private handDisplay handDisplay;
+
+    private Dealer() {
+
+    }
+
+    private void shuffle() {
+
+    }
+
+    private void initialDeal() {
+
+    }
+
+    private void deal() {
+
+    }
+
+}
