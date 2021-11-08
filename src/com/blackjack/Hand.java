b0583e2da87303d9f4751e8cab7b23bc5c6ad9c7
Armando Guardado <armando.guardado.ag@gmail.com>
2021-11-07 18:33:55 -0800

    Created hand class, set up getter and setters, and two business methods.

---
+++ src/com/blackjack/Hand.java
@@ -0,0 +1,85 @@
+package com.blackjack;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Hand {
+
+    private Map<String,Card> cards = new HashMap<>();
+    private List<Integer> arrayValues;
+    private String cardImages;
+    private Integer handScore;
+    
+    
+    //Business methods 
+    
+    public void updateInfo(){
+        List<Integer> result = new ArrayList<Integer>();
+        Integer sum = 0;
+        for (Map.Entry<String, Card> entry: getCards().entrySet()) {
+            Card card = entry.getValue();
+            result.add(card.getValue());
+
+            sum += card.getValue();
+        }
+        setArrayValues(result);
+        setHandScore(sum);
+    }
+
+    public Integer calculateScore(){
+        int result = getHandScore();
+
+        if(getHandScore() == 21 && getArrayValues().size() == 2){
+            result = 0;
+        }
+        if(getArrayValues().contains(11) && getHandScore() > 21){
+            int returnIndex = getArrayValues().indexOf(11);
+            getArrayValues().set(returnIndex, 1);
+        }
+        setHandScore(result);
+
+        return result;
+    }
+    
+    
+    
+    
+    
+    
+    //Accessor Methods
+
+    public Map<String, Card> getCards() {
+        return cards;
+    }
+
+    public void setCards(Map<String, Card> cards) {
+        this.cards = cards;
+    }
+
+    public List<Integer> getArrayValues() {
+        return arrayValues;
+    }
+
+    public void setArrayValues(List<Integer> arrayValues) {
+        this.arrayValues = arrayValues;
+    }
+
+    public String getCardImages() {
+        return cardImages;
+    }
+
+    public void setCardImages(String cardImages) {
+        this.cardImages = cardImages;
+    }
+
+    public Integer getHandScore() {
+        return handScore;
+    }
+
+    public void setHandScore(Integer handScore) {
+        this.handScore = handScore;
+    }
+    
+}
