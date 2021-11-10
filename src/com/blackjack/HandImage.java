6597df415c59c3ec5696b4681b03640c23dc1998
Armando Guardado <armando.guardado.ag@gmail.com>
2021-11-10 09:12:00 -0800

    Updated setter and getters for CardImage variable in the HandImage class and adjusted name of variables

--- src/com/blackjack/HandImage.java
+++ src/com/blackjack/HandImage.java
@@ -8,7 +8,7 @@ import java.util.Map;
 public class HandImage {
     String cardSymbol;
     List<StringBuilder> cardImage = new ArrayList<>();
-    Map<String, String[]> tempaltes = new HashMap<>();
+    Map<String, String[]> suitTemplates = new HashMap<>();
 
 
     public HandImage() {
@@ -22,20 +22,8 @@ public class HandImage {
     }
 
 
-
-//    String[] template = new String[]{
-//            ".------.\t" + "",
-//            "|" + getCardSymbol() + ".--. |\t" + "",
-//            "| (\\/) |\t" + "",
-//            "| :\\/: |\t" + "",
-//            "| '--'" + getCardSymbol() + "|\t" + "",
-//            "`------'\t" + ""
-//
-//    };
-
-
     public void createHand(String symbol, String suit) {
-        String[] template = getTempaltes().get(suit);
+        String[] template = getSuitTemplates().get(suit);
         setCardSymbol(symbol);
         for (int i = 0; i < cardImage.size(); i++) {
             if (i == 1) {
@@ -50,12 +38,6 @@ public class HandImage {
 
     }
 
-    public void printHand() {
-        for (StringBuilder row : cardImage) {
-            System.out.println(row);
-
-        }
-    }
 
     private void initializeSuitTemplates() {
         String[] heartsTemplate = new String[]{
@@ -95,11 +77,19 @@ public class HandImage {
                 "| '--'" + getCardSymbol() + "|\t" + "",
                 "`------'\t" + ""
         };
-        getTempaltes().put("hearts", heartsTemplate);
-        getTempaltes().put("clubs", clubsTemplate);
-        getTempaltes().put("spades", spadesTemplate);
-        getTempaltes().put("diamonds", diamondsTemplate);
+        getSuitTemplates().put("hearts", heartsTemplate);
+        getSuitTemplates().put("clubs", clubsTemplate);
+        getSuitTemplates().put("spades", spadesTemplate);
+        getSuitTemplates().put("diamonds", diamondsTemplate);
 
+    }
+
+    public List<StringBuilder> getCardImage() {
+        return cardImage;
+    }
+
+    public void setCardImage(List<StringBuilder> cardImage) {
+        this.cardImage = cardImage;
     }
 
     public String getCardSymbol() {
@@ -110,11 +100,11 @@ public class HandImage {
         this.cardSymbol = cardSymbol;
     }
 
-    public Map<String, String[]> getTempaltes() {
-        return tempaltes;
+    public Map<String, String[]> getSuitTemplates() {
+        return suitTemplates;
     }
 
-    public void setTempaltes(Map<String, String[]> tempaltes) {
-        this.tempaltes = tempaltes;
+    public void setSuitTemplates(Map<String, String[]> suitTemplates) {
+        this.suitTemplates = suitTemplates;
     }
 }
