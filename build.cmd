283c2f220f8d30134e8b939208852473c131dcb0
redrianhood <rian.boren@gmail.com>
2021-11-10 14:03:54 -0800

    build & edit to run for distro

---
+++ build.cmd
@@ -0,0 +1,7 @@
+@echo off
+
+rem     Compile the application code
+javac -c classes src\com\blackjack\*.java src\com\blackjack\client\*.java src\com\blackjack\controller\*.java
+
+rem     Build the application JAR
+jar --create --file BlackJackBets-1.0.jar -C classes .
