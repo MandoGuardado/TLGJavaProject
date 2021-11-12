fd66a14ea343e1433d7c1f26fdd32c3312f81f93
redrianhood <rian.boren@gmail.com>
2021-11-11 20:43:28 -0800

    adjust filenames

--- build.cmd
+++ build.cmd
@@ -4,4 +4,4 @@ rem     Compile the application code
 javac -d classes src\com\blackjack\*.java src\com\blackjack\client\*.java src\com\blackjack\controller\*.java
 
 rem     Build the application JAR
-jar --create --file BlackJackBets-1.0.jar -C classes .
+jar --create --file BlackJackBets-0.1.jar -C classes .
